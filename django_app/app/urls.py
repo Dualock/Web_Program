bbb5abd87e414d803c3a6d6f1afc37591a953744
djfonsecamo <djfonseca@gmail.com>
2021-06-06 06:23:09 -0700

    New visual styles and functionality added

--- django_app/app/urls.py
+++ django_app/app/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('estadios/', views.estadios, name='estadios'),
     path('estadio/<int:estadio_id>/', views.estadio, name='estadio'),
-    path('log_in/', views.log_in),
-    path('eventos', views.eventos),
+    path('log_in/', views.log_in, name='log_in'),
+    path('eventos/', views.eventos, name='eventos'),
 ]
