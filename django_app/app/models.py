a5e11b003c10578f40fa5035cba00b9a425979b1
Emilio Rojas <emilio93@gmail.com>
2021-06-09 19:16:12 -0600

    Add new models

--- django_app/app/models.py
+++ django_app/app/models.py
@@ -1,6 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Estadio(models.Model):
     nombre = models.CharField(max_length=200)
-    capacidad = models.IntegerField(default=0)
+    descripción = models.TextField
+
+class ImagenEstadio(models.Model):
+    estadio = models.ForeignKey(Estadio, on_delete=models.CASCADE)
+    url = models.CharField(max_length=200)
+    altText = models.CharField(max_length=200, default="Imagen de Estadio")
+
+class ImagenUsuario(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    url = models.CharField(max_length=200)
+    altText = models.CharField(max_length=200, default="Imagen de Usuario")
+
+class Partido(models.Model):
+    estadio = models.ForeignKey(Estadio, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=200)
+    inicio = models.DateTimeField()
+    fin = models.DateTimeField()
+
+class ImagenPartido(models.Model):
+    partido = models.ForeignKey(Partido, on_delete=models.CASCADE)
+    url = models.CharField(max_length=200)
+    altText = models.CharField(max_length=200, default="Imagen de Usuario")
+
+class TipoAsiento(models.Model):
+    estadio = models.ForeignKey(Estadio, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=200)
+    costo = models.FloatField()
+    capacidad = models.IntegerField()
+    descripcion = models.TextField()
+
+class Reserva(models.Model):
+    partido = models.ForeignKey(Partido, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tipoAsiento = models.ForeignKey(TipoAsiento, on_delete=models.CASCADE)
