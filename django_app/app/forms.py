0c769c0bc2d8bdf7300d00766fbe7ab48c1e8998
Emilio Rojas <emilio93@gmail.com>
2021-06-11 02:22:32 -0600

    Add forms for admin panel

--- django_app/app/forms.py
+++ django_app/app/forms.py
@@ -10,3 +10,17 @@ class SignupForm(forms.Form):
     username = forms.CharField(label='Usuario', max_length=100)
     email = forms.CharField(label='Email', max_length=100, widget=forms.EmailInput)
     password = forms.CharField(label='Clave', max_length=100, widget=forms.PasswordInput)
+
+
+class AdminSignupForm(forms.Form):
+    nombre = forms.CharField(label='Nombre', max_length=100)
+    apellido = forms.CharField(label='Apellido', max_length=100)
+    username = forms.CharField(label='Usuario', max_length=100)
+    email = forms.CharField(label='Email', max_length=100, widget=forms.EmailInput)
+    password = forms.CharField(label='Clave', max_length=100, widget=forms.PasswordInput)
+    isStaff = forms.BooleanField(label="Es staff")
+
+class CreateEstadioForm(forms.Form):
+    nombre = forms.CharField(label='Nombre', max_length=100)
+    imagen = forms.FileField()
+    descripción = forms.CharField(widget=forms.Textarea)
