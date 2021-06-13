7a3435f69ef7b287d74f51fc07137de205862a08
Emilio Rojas <emilio93@gmail.com>
2021-06-12 23:29:11 -0600

    Add ending slash to url

--- django_app/app/urls.py
+++ django_app/app/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path('admin/', views.admin, name='admin'),
     path('crearEstadio/', views.create_estadio, name='crear_estadio'),
     path('perfil/<str:user_username>/', views.perfil_view, name = 'perfil'),
-    path('crearUsuarioAdmin', views.create_admin, name = 'crear_user_admin')
+    path('crearUsuarioAdmin/', views.create_admin, name = 'crear_user_admin')
 ]
